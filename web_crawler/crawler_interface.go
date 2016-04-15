eec2f67d1225699669a1dd528c8a86c91260077f
astinchoi <asciineo@gmail.com>
2016-04-15 21:44:41 +0900

    following and star complete

--- web_crawler/crawler_interface.go
+++ web_crawler/crawler_interface.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"log"
-	"net/html"
+	"net/http"
 	"runtime"
 	"sync"
 )
@@ -18,11 +18,13 @@ type StarsResult struct {
 	repo string
 }
 
+// duplicated urls
 type FetchedUrl struct {
 	m map[string]error
 	sync.Mutex
 }
 
+// duplicated Repos
 type FetchedRepo struct {
 	m map[string]struct{}
 	sync.Mutex
@@ -48,10 +50,26 @@ type GitHubFollowing struct {
 
 type GitHubStars struct {
 	fetchedUrl 	*FetchedUrl
-	fechedRepo  *FechedRepo
+	fetchedRepo  *FetchedRepo
 	p 			*Pipeline
 	result 		chan StarsResult
 	url 		string
+}
+
+func fetch(url string) (*html.Node, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return doc, nil
 }
 
 func (g *GitHubFollowing) Request(url string) {
@@ -64,23 +82,13 @@ func (g *GitHubFollowing) Request(url string) {
 	}
 }
 
-func (g *GitHubStars) Request(url string) {
-	g.p.request <- &GitHubStars {
-		fetchedUrl:	 g.fetchedUrl,
-		fetchedRepo: g.fetchedRepo,
-		p:			 g.p,
-		result:		 g.result,
-		url:		 url,
-	}
-}
-
 func (g *GitHubFollowing) Parse(doc *html.Node) <-chan string {
 	name := make(chan string)
 	go func() {
 		var f func(*html.Node)
 		f = func(n *html.Node) {
 			if n.Type == html.ElementNode && n.Data == "img" {
-				for _, a := rnage n.Attr {
+				for _, a := range n.Attr {
 					if a.Key == "class" && a.Val == "avatar left" {
 						for _, a := range n.Attr {
 							if a.Key == "alt" {
@@ -108,4 +116,169 @@ func (g *GitHubFollowing) Parse(doc *html.Node) <-chan string {
 	}()
 
 	return name
+}
+
+func (g *GitHubStars) Request(url string) {
+	g.p.request <- &GitHubStars {
+		fetchedUrl:	 g.fetchedUrl,
+		fetchedRepo: g.fetchedRepo,
+		p:			 g.p,
+		result:		 g.result,
+		url:		 url,
+	}
+}
+
+func (g *GitHubFollowing) Crawl() {
+	g.fetchedUrl.Lock()
+	if _, ok := g.fetchedUrl.m[g.url]; ok {
+		g.fetchedUrl.Unlock()
+		return
+	}
+	g.fetchedUrl.Unlock()
+
+	doc, err := fetch(g.url)
+	if err != nil {
+		go func(u string) {
+			g.Request(u)
+		}(g.url)
+		return
+	}
+
+	g.fetchedUrl.Lock()
+	g.fetchedUrl.m[g.url] = err
+	g.fetchedUrl.Unlock()
+
+	name := <-g.Parse(doc)
+	g.result <- FollowingResult{g.url, name}
+}
+
+func (g *GitHubStars) Parse(doc *html.Node) <-chan string {
+	repo := make(chan string)
+
+	go func() {
+		defer close(repo)
+
+		var f func(*html.Node)
+		f = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" {
+				for _, a := range n.Attr {
+					if a.Key == "class" && a.Val == "prefix" {
+						repo <- n.Parent.Attr[0].Val
+						break
+					}
+				}
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		f(doc)
+	}()
+
+	return repo
+}
+
+func (g *GitHubStars) Crawl() {
+	g.fetchedUrl.Lock()
+	if _, ok := g.fetchedUrl.m[g.url]; ok {
+		g.fetchedUrl.Unlock()
+		return
+	}
+	g.fetchedUrl.Unlock()
+
+	doc, err := fetch(g.url)
+	if err != nil {
+		go func(u string) {
+			g.Request(u)
+		}(g.url)
+		return
+	}
+
+	g.fetchedUrl.Lock()
+	g.fetchedUrl.m[g.url] = err
+	g.fetchedUrl.Unlock()
+
+	repositories := g.Parse(doc)
+	for r := range repositories {
+		g.fetchedRepo.Lock()
+		if _, ok := g.fetchedRepo.m[r]; !ok {
+			g.result <- StarsResult{r}
+			g.fetchedRepo.m[r] = struct{}{}
+		}
+		g.fetchedRepo.Unlock()
+	}
+}
+
+func NewPipeline() *Pipeline {
+	return &Pipeline {
+		request: make(chan Crawler),
+		done:	 make(chan struct{}),
+		wg:		 new(sync.WaitGroup),
+	}
+}
+
+func (p *Pipeline) Run() {
+	const numWorkers = 10
+	p.wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			p.Worker()
+			p.wg.Done()
+		}()
+	}
+
+	go func() {
+		p.wg.Wait()
+	}()
+}
+
+func (p *Pipeline) Worker() {
+	for r := range p.request {
+		select {
+		case <-p.done:
+			return
+		default:
+			r.Crawl()
+		}
+	}
+}
+
+func main() {
+	numCPUs := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPUs)
+
+	p := NewPipeline()
+	p.Run()
+
+	stars := &GitHubStars{
+		fetchedUrl:  &FetchedUrl{m: make(map[string]error)},
+		fetchedRepo: &FetchedRepo{m: make(map[string]struct{})},
+		p:			 p,
+		result:		 make(chan StarsResult),
+	}
+	following := &GitHubFollowing {
+		fetchedUrl: &FetchedUrl{m: make(map[string]error)},
+		p:			p,
+		result:		make(chan FollowingResult),
+		stars:		stars,
+		url:		"https://github.com/astinchoi/following",
+	}
+	p.request <- following
+
+	count := 0
+
+	LOOP:
+	for {
+		select {
+			case f := <-following.result:
+				fmt.Println(f.name)
+			case s := <-stars.result:
+				fmt.Println(s.repo)
+				if count == 1000 {
+					close(p.done)
+					break LOOP
+				}
+				count++
+		}
+	}
 }
